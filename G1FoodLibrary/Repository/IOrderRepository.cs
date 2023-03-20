171de451023e0f74a4c95d78f1ba96486bd35a74
NghiaHH28 <nghiahh28@gmail.com>
2023-03-20 11:34:38 +0700

    Start

---
+++ G1FoodLibrary/Repository/IOrderRepository.cs
@@ -0,0 +1,19 @@
+﻿using DataAccess.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G1FoodLibrary.Repository
+{
+    public interface IOrderRepository
+    {
+        public void CreateOrder(OrderDTO orderDTO);
+        public void CreateOrderDetails(OrderDTO orderDTO);
+        public OrderDTO GetOrderByOrderID(string orderID);
+        public IEnumerable<OrderDetailsDTO> GetListOrderDetailsByOrderID(string orderID);
+        public IEnumerable<OrderDTO> GetListOrderByAccountID(string accountID);
+        public IEnumerable<OrderDTO> GetAllOrder();
+    }
+}
---
+++ G1FoodLibrary/Repository/OrderRepository.cs
@@ -0,0 +1,25 @@
+﻿using DataAccess.DTO;
+using G1FoodLibrary.DAO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G1FoodLibrary.Repository
+{
+    public class OrderRepository : IOrderRepository
+    {
+        public void CreateOrder(OrderDTO orderDTO) => OrderDAO.Instance.AddOrder(orderDTO);
+
+        public void CreateOrderDetails(OrderDTO orderDTO) => OrderDAO.Instance.AddOrderDetails(orderDTO);
+
+        public IEnumerable<OrderDTO> GetAllOrder() => OrderDAO.Instance.GetAllOrder();
+
+        public IEnumerable<OrderDTO> GetListOrderByAccountID(string accountID) => OrderDAO.Instance.GetAllOrderByAccountID(accountID);
+
+        public IEnumerable<OrderDetailsDTO> GetListOrderDetailsByOrderID(string orderID) => OrderDAO.Instance.GetAllOrderDetailsByOrderID(orderID);
+
+        public OrderDTO GetOrderByOrderID(string orderID) => OrderDAO.Instance.GetAllOrderByOrderID(orderID);
+    }
+}
