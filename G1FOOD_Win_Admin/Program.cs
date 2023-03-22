a827b7f0bcede98876757453aafe1d04b1252c55
NghiaHH28 <nghiahh28@gmail.com>
2023-03-22 21:29:40 +0700

    Hi

--- G1FOOD_Win_Admin/Program.cs
+++ G1FOOD_Win_Admin/Program.cs
@@ -1,3 +1,4 @@
+using MyStoreWinApp;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,7 +18,7 @@ namespace G1FOOD_Win_Admin
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new frmLogin());
         }
     }
 }
